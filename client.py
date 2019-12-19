3ce2c8411f688308f682a2c5b6d43779f1b0afd3
johhuang1 <375432636@example.com>
2019-12-19 17:14:24 +0800

    add the client.py

---
+++ client.py
@@ -0,0 +1,29 @@
+from socket import *
+
+class soc():
+    def __init__(self):
+        self.flag = False
+    def MakeConnect(self):
+        ipaddr = '10.3.141.56'
+        port = 8009
+        try:
+            self.tcp_client = socket(AF_INET,SOCK_STREAM)
+            self.tcp_client.connect((ipaddr, port))
+            self.flag = True
+        except ConnectionRefusedError:
+            self.flag = False
+
+    def SendMessage(self):
+        try:
+            self.tcp_client.send(self.message.encode("utf-8"))
+            data = self.tcp_client.recv(1024)
+            print("data is %s" %data.decode("utf-8"))
+        except ConnectionRefusedError:
+            self.flag = False
+    def TryToSend(self,message):
+        self.message = message
+        if self.flag:
+            self.SendMessage()
+        else:
+            self.MakeConnect()
+            self.SendMessage()
